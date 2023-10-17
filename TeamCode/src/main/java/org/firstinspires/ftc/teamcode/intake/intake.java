0544744585a24a9ba1d3806679cf4108688715c4
Sveta Lunnova <lunnovas@icloud.com>
2023-10-17 16:53:35 +0300

    Created intake class and prop_blue_detector class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/intake.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.intake;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "intake_test")
+public class intake extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
