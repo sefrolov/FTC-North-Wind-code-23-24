76c3c9db6e1de23ceaa0e48a24dc23c029f23f3a
Sveta Lunnova <lunnovas@icloud.com>
2023-10-19 17:10:13 +0300

    Update intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/intake.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name = "intake_test")
 public class intake extends LinearOpMode {
     DcMotor motor1 = null;
-    DcMotor motor2 = null;
+     DcMotor motor2 = null;
     BNO055IMU imu;
 
     @Override
@@ -21,22 +21,24 @@ public class intake extends LinearOpMode {
         motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         // init of motor2
-        motor2 = hardwareMap.get(DcMotor.class, "motor2");
-        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
-        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+         motor2 = hardwareMap.get(DcMotor.class, "motor2");
+         motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+         motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+         motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
         while (opModeIsActive()) {
-
+//
+            //
+            //
             if ((Math.abs(gamepad1.right_stick_y) >= 0.1) && opModeIsActive()) {
                 double Power = 0;
 
                 Power = gamepad1.right_stick_y * 0.25;
                 motor1.setPower(Power);
-                motor2.setPower(Power);
+                 motor2.setPower(Power);
 
                 // Show the elapsed game time and wheel power.
                 //  telemetry.addData("Status", "Run Time: " + runtime.toString());
