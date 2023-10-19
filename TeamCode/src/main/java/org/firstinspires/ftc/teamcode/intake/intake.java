55b4783d6356c7704122d48ff5bf8834a98201bd
Sveta Lunnova <lunnovas@icloud.com>
2023-10-19 16:45:10 +0300

    Update intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/intake.java
@@ -8,25 +8,35 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "intake_test")
 public class intake extends LinearOpMode {
-    DcMotor motor = null;
+    DcMotor motor1 = null;
+    DcMotor motor2 = null;
     BNO055IMU imu;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        motor = hardwareMap.get(DcMotor.class, "motor");
-        motor.setDirection(DcMotorSimple.Direction.FORWARD);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // init of motor1
+        motor1 = hardwareMap.get(DcMotor.class, "motor1");
+        motor1.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // init of motor2
+        motor2 = hardwareMap.get(DcMotor.class, "motor2");
+        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         waitForStart();
 
         while (opModeIsActive()) {
 
             if ((Math.abs(gamepad1.right_stick_y) >= 0.1) && opModeIsActive()) {
-                double leftPower = 0;
+                double Power = 0;
 
-                leftPower = gamepad1.right_stick_y * 0.25;
-                motor.setPower(leftPower);
+                Power = gamepad1.right_stick_y * 0.25;
+                motor1.setPower(Power);
+                motor2.setPower(Power);
 
                 // Show the elapsed game time and wheel power.
                 //  telemetry.addData("Status", "Run Time: " + runtime.toString());
